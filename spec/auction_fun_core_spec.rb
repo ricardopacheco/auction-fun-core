644529c37ae0d5e83157b41c9c19479066d73d5f
Ricardo Pacheco <innervisiondev@gmail.com>
2024-02-24 17:58:49 -0300

    Fix specs

--- spec/auction_fun_core_spec.rb
+++ spec/auction_fun_core_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe AuctionFunCore do
   it "has a version number" do
-    expect(AuctionFunCore::VERSION).to eq("0.5.0")
+    expect(AuctionFunCore::VERSION).to eq("0.6.0")
   end
 end
